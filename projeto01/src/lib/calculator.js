32a49dbe2da3b2a53c6c867d9fa82cd4e060df20
Juliana <aragaopintojuli@gmail.com>
2022-03-19 21:10:18 -0300

    finish sum method with TDD

--- projeto01/src/lib/calculator.js
+++ projeto01/src/lib/calculator.js
@@ -1,3 +1,9 @@
 module.exports.sum = (num1, num2) => {
-  return num1 + num2;
+  const int1 = parseInt(num1);
+  const int2 = parseInt(num2);
+
+  if (Number.isNaN(int1) || Number.isNaN(int2))
+    throw new Error('Input is not a number, please check out');
+
+  return int1 + int2;
 };
